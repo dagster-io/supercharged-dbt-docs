591d69428610ae97b150cf0fab8fb69eef22adcf
Marco Salazar <marco@elementl.com>
2023-07-26 15:30:51 -0400

    rm type conversion

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,27 +1,10 @@
-import { Catalog, Convert as CatalogConvert } from "@/schemas/catalog";
-import { Manifest, Convert as ManifestConvert } from "@/schemas/manifest";
 import rawCatalogData from "@/dbt-project-data/catalog.json";
 import rawManifestData from "@/dbt-project-data/manifest.json";
 
-let _catalog: Catalog;
 export function getCatalog() {
-  if (!_catalog) {
-    _catalog = CatalogConvert.toCatalog(JSON.stringify(rawCatalogData));
-  }
-  return _catalog;
+  return rawCatalogData;
 }
 
-let _manifest: Manifest;
 export function getManifest() {
-  if (!_manifest) {
-    _manifest = ManifestConvert.toManifest(JSON.stringify(rawManifestData));
-  }
-  return _manifest;
-}
-
-let _project: any;
-export function getProject() {
-  if (!_project) {
-  }
-  return _project;
+  return rawManifestData;
 }
