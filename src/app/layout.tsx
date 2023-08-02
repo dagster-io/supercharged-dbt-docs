c75f0606a19468a29df4e610c46b9df985013a4a
Marco Salazar <marco@elementl.com>
2023-08-02 18:48:41 -0400

    preload treedata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preload" href="/treedata" type="application/json" />
       </head>
       <body className={inter.className}>
         <main className="mx-8">
